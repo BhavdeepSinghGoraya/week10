49f8122b902bb38e6f473f3c8724b572719a966a
Bhavdeep Singh <gorayabhavdeep@gmail.com>
2022-11-08 16:13:39 -0800

    Activity

--- scripts/script.js
+++ scripts/script.js
@@ -38,3 +38,57 @@ while (!isNaN(Number(input)));
 
 // alert('HELLO  ' + input)
 alert(`HELLO! ${input}`)
+
+let fruits = ['Apple','Banana'];
+fruits.push('Orange');
+fruits.pop();
+fruits.unshift('Mango');
+fruits.shift()
+
+// let sequence = [1,1,2,3,5,8,13];
+
+// for (let i = 0 ; i < sequence.length ; i = i+1)
+// {
+//      i / 2 != 0; 
+//     console.log(i);
+// }
+
+const person = {
+    name:['Bob', 'Smith'],
+    age:32,
+    gender:'male',
+    hobbies :['music', 'skiing']
+}
+
+console.log(`my name is ${person.name[0]} ${person.name[1]}. I am interseted in ${person.hobbies[0]} and ${person.hobbies[1]}`);
+
+for (let key in person){
+    console.log(key);          //Returns a key and elements in the keys 
+    console.log(person[key]);
+}
+let student1 = {
+    ID: "A00022",
+    GPA: 3.0,
+    program: "CIT"
+    };
+let student2 = {
+    ID: "A01000",
+    GPA: 3.1,
+    program: "CST"
+    };
+let student3 = {
+    ID: "A00114",
+    GPA: 3.2,
+    program: "CIT"
+    };
+let students = [student1, student2, student3];
+let count = 0;   
+for (let student in students){
+    if (student.GPA > 3.0 && student.program == "CIT")
+    {
+        count ++;
+    console.log(student)
+}
+
+
+}
